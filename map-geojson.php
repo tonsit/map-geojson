dc92295327da7d2ea70181aa2ca6d5449a47c742
tonsit <tonsit@gmail.com>
2013-08-23 18:35:22 -0400

    Updated wrapper functions
    SQL insert and query

--- map-geojson.php
+++ map-geojson.php
@@ -45,8 +45,8 @@ register_activation_hook( __FILE__, 'sabra_maps_create_tables' );
 /* list all table columns here for whitelisting */
 function sabra_maps_get_table_columns() {
 	return array(
-       'id'=> '%d',
-        'post_id'=> '%d',
+		'id'=>'%d',
+        'post_id'=>'%d',
         'item_id'=>'%d',
         'date_created'=>'%s',
 		'date_active'=>'%s',
@@ -56,40 +56,147 @@ function sabra_maps_get_table_columns() {
     );
 }
 
-function sabra_maps_insert( $data=array() ){
+function sabra_maps_insert( $data = array(), $timestamp = false, $country_id = false ){
     global $wpdb;        
- 
+	$current_timestamp = current_time('timestamp');
+	$current_timestamp = date_i18n( 'Y-m-d H:i:s', $current_timestamp, true );
+
+	var_dump( $data );
+	
     //Set default values
-    $data = wp_parse_args($data, array(
-                 'date'=> current_time('timestamp'),
-    ));    
- 
-    //Check date validity
-    if( !is_float($data['date']) || $data['date'] <= 0 )
-        return 0;
  
     //Convert activity date from local timestamp to GMT mysql format
-    $data['activity_date'] = date_i18n( 'Y-m-d H:i:s', $data['date'], true );
+	if ( !$timestamp ) {
+		$active_timestamp = $current_timestamp;
+	}
+	else {
+		$active_timestamp = strtotime($timestamp);
+		$active_timestamp = date_i18n( 'Y-m-d H:i:s', $active_timestamp, true );
+	}
+	//if ( !$country_id )
+	
+	//Query
+	$current_queries = sabra_maps_query( current_time('timestamp'), $country_id, $data, 'ARRAY_A');
+	
+	echo "<hr>";
+	var_dump($current_queries);
+	echo "<hr>";
+	
+	// convert array	newarray [post_id][item_id]
+	$data_array = array();
+	
+	foreach( $current_queries as $query ) {
+	
+		if ( isset( $data_array[ $query['post_id'] ] ) ) {
+		
+			if ( !isset( $data_array[ $query['post_id'] ][ $query['item_id'] ] ) ) {
+				$data_array[ $query['post_id'] ][ $query['item_id'] ] = $query;
+			} else {
+				//Duplicate Entry
+			}
+		
+		} else {
+		
+			$data_array[$query['post_id']][$query['item_id']] = $query;
+		
+		}
+	
+	}
+	
+	var_dump( $data_array );
+	
+	$inserts = array();
+	$updates = array();
+	
+	$column_formats = sabra_maps_get_table_columns();
+	$column_names_array = array();
+	foreach( $column_formats as $name => $format ) {
+		if ( $name != 'id' ) $column_names_array[] = $name;
+	}
+	$column_names = implode( ', ', $column_names_array );
+	
+	foreach( $data as $entry ) {
+
+		if ( $entry['post_id'] == '' || $entry['item_id'] == '' || ( '' == $entry['value1'] || '' == $entry['value2'] ) ) continue;
+	
+		//Initialise column format array
+		$column_formats = sabra_maps_get_table_columns();
+	
+		//Force fields to lower case
+		$entry = array_change_key_case ( $entry );
+	 
+		//White list columns
+		$entry = array_intersect_key($entry, $column_formats);
+	 
+		//Reorder $column_formats to match the order of columns given in $data
+		$entry_keys = array_keys($entry);
+		$column_formats = array_merge(array_flip($entry_keys), $column_formats);
+	
+		if ( isset( $data_array[ $entry['post_id'] ][ $entry['item_id'] ] ) ) {
+			// Match Found
+			$updates[] = $wpdb->prepare("
+					UPDATE $wpdb->sabra_maps 
+					SET date_replaced = %s 
+					WHERE id = %d",
+					$current_timestamp, $data_array[ $entry['post_id'] ][ $entry['item_id'] ]['id'] );
+						
+			
+		} else {
+		
+			// New Entry!
+			
+		
+		}
+		
+		$inserts[] = $wpdb->prepare("
+					INSERT INTO $wpdb->sabra_maps ($column_names)
+					VALUES (%d, %d, %s, %s, NULL, %s, %s)",
+					$entry['post_id'], $entry['item_id'], $current_timestamp, $active_timestamp, $entry['value1'], $entry['value2'] ); 
+	
+	}
+	
+	//Declare insert/updates
+	
+	//Compare Query to New Values
+
+	//Loop through new values
+
+	//Check value1 - value2 against existing
+
+	//if different populate $inserts[] and $updates[]
+
+	
+	
+    $data = wp_parse_args($data, array(
+		'date_created'=> $current_timestamp,
+		'date_active'=> $active_timestamp
+	 ));    
+
+	$sql = '';
+	$insertsql = implode( ", ", $inserts);
+	$sql .= ', ';
+	$updatesql = implode( ', ', $updates);
+	
+	echo "<hr>";
+	
+	$wpdb->show_errors();
+	
+	var_dump( $sql );
+	
+	if( !$wpdb->query( $insertsql ) )
+         return false;
+	if( !$wpdb->query( $updatesql ) )
+         return false;
+    
+    //$wpdb->insert($wpdb->sabra_maps, $data, $column_formats);
  
-    //Initialise column format array
-    $column_formats = sabra_maps_get_table_columns();
+	
  
-    //Force fields to lower case
-    $data = array_change_key_case ( $data );
- 
-    //White list columns
-    $data = array_intersect_key($data, $column_formats);
- 
-    //Reorder $column_formats to match the order of columns given in $data
-    $data_keys = array_keys($data);
-    $column_formats = array_merge(array_flip($data_keys), $column_formats);
- 
-    $wpdb->insert($wpdb->sabra_maps, $data, $column_formats);
- 
-    return $wpdb->insert_id;
+    //return $wpdb->insert_id;
+	return null;
 }
 
-function sabra_maps_update( $map_id, $data=array() ){
+function sabra_maps_update( $post_id, $data=array() ){
     global $wpdb;        
  
     //Log ID must be positive integer
@@ -120,13 +227,20 @@ function sabra_maps_update( $map_id, $data=array() ){
     return true;
 }
 
-function sabra_maps_query( $query=array() ){
- 
-     global $wpdb;
-     /* Parse defaults */
+function sabra_maps_query( $timestamp = false, $country_id = false, $query = array(), $output_type = 'OBJECT_K' ){
+    global $wpdb;
+	if ( !$timestamp ) 
+		$timestamp = current_time('timestamp');
+
+
+	/* Parse defaults */
      $defaults = array(
-       'fields'=>array(),'orderby'=>'datetime','order'=>'desc', 'user_id'=>false,
-       'since'=>false,'until'=>false,'number'=>10,'offset'=>0
+       'fields'		=>	array(),
+	   'orderby'	=>	'post_id',
+	   'order'		=>	'desc',
+	   'number'		=>	3,
+	   'offset'		=>	0,
+	   'type'		=>	'point_in_time'
      );
  
     $query = wp_parse_args($query, $defaults);
@@ -158,7 +272,7 @@ function sabra_maps_query( $query=array() ){
  
     //Return only selected fields. Empty is interpreted as all
     if( empty($fields) ){
-        $select_sql = "SELECT* FROM {$wpdb->sabra_maps}";
+        $select_sql = "SELECT * FROM {$wpdb->sabra_maps}";
     }elseif( 'count' == $fields ) {
         $select_sql = "SELECT COUNT(*) FROM {$wpdb->sabra_maps}";
     }else{
@@ -173,28 +287,12 @@ function sabra_maps_query( $query=array() ){
     //Initialise WHERE
     $where_sql = 'WHERE 1=1';
  
-    if( !empty($post_id) )
-       $where_sql .=  $wpdb->prepare(' AND post_id=%d', $post_id);
+    if( !empty($country_id) )
+       $where_sql .=  $wpdb->prepare(' AND post_id=%d', $country_id);
+
  
-    if( !empty($user_id) ){
- 
-       //Force $user_id to be an array
-       if( !is_array( $user_id) )
-           $user_id = array($user_id);
- 
-       $user_id = array_map('absint',$user_id); //Cast as positive integers
-       $user_id__in = implode(',',$user_id);
-       $where_sql .=  " AND user_id IN($user_id__in)";
-    }
- 
-    $since = absint($since);
-    $until = absint($until);
- 
-    if( !empty($since) )
-       $where_sql .=  $wpdb->prepare(' AND activity_date >= %s', date_i18n( 'Y-m-d H:i:s', $since, true));
- 
-    if( !empty($until) )
-       $where_sql .=  $wpdb->prepare(' AND activity_date <= %s', date_i18n( 'Y-m-d H:i:s', $until, true));
+   $where_sql .=  $wpdb->prepare(' AND date_active <= %s', date_i18n( 'Y-m-d H:i:s', $timestamp, true));
+   $where_sql .=  $wpdb->prepare(' AND ((date_replaced IS NULL) OR (date_replaced > %s))', date_i18n( 'Y-m-d H:i:s', $timestamp, true));
  
     /* SQL Order */
     //Whitelist order
@@ -205,11 +303,8 @@ function sabra_maps_query( $query=array() ){
        case 'post_id':
             $order_sql = "ORDER BY post_id $order";
        break;
-       case 'user_id':
-            $order_sql = "ORDER BY user_id $order";
-       break;
        case 'datetime':
-             $order_sql = "ORDER BY activity_date $order";
+             $order_sql = "ORDER BY date_active $order";
        default:
        break;
     }
@@ -237,17 +332,18 @@ function sabra_maps_query( $query=array() ){
     }
  
     /* Perform query */
-    $logs = $wpdb->get_results($sql);
+    $maps = $wpdb->get_results($sql, $output_type);
  
     /* Add to cache and filter */
     wp_cache_add( $cache_key, $logs, 24*60*60 );
-    $logs = apply_filters('sabra_maps_query', $logs, $query);
-    return $logs;
- }
+    $maps = apply_filters('sabra_maps_query', $maps, $query);
+    return $maps;
+}
+
  function sabra_maps_delete( $post_id ){
     global $wpdb;        
  
-    //Log ID must be positive integer
+    //post_id must be positive integer
     $post_id = absint($post_id);     
     if( empty($post_id) )
          return false;
